dd1c7ca762a654a3b4e9342e83cf128f5aacd889
Davis Allen <davis.b.allen@gmail.com>
2017-07-31 13:40:48 -0400

    Cleanup unused reference to other components of IDML package

--- src/test.py
+++ src/test.py
@@ -1,35 +1,13 @@
 import os
 import xml.etree.ElementTree as ET
 
-# directory_in_str = "/Users/davisallen/Projects/Python/idmlToHtml/assets/Example_IDML_File/"
-# directory = os.fsencode(directory_in_str)
 directory = "/Users/davisallen/Projects/Python/idmlToHtml/assets/Example_IDML_File/"
 output_html_file = "/Users/davisallen/Projects/Python/idmlToHtml/output/testoutput/index.html"
 
-design_map = ""
-spreads = []
-master_spreads = []
 stories = []
 
 for item in os.listdir(directory):
-    if item == "designmap.xml":
-        print(item)
-        design_map = item
-    elif item == "Spreads":
-        print(item)
-        subdir = directory + item
-        subitems = os.listdir(subdir)
-        for subitem in subitems:
-            print("    " + subitem)
-        spreads = subitems
-    elif item == "MasterSpreads":
-        print(item)
-        subdir = directory + item
-        subitems = os.listdir(subdir)
-        for subitem in subitems:
-            print("    " + subitem)
-            master_spreads = subitems
-    elif item == "Stories":
+    if item == "Stories":
         print(item)
         subdir = directory + item
         subitems = os.listdir(subdir)
@@ -37,28 +15,8 @@ for item in os.listdir(directory):
             print("    " + subitem)
         stories = subitems
 
-print("************************")
-
-spread_xml_trees = dict()
-spread_xml_roots = dict()
-
-master_spread_xml_trees = dict()
-master_spread_xml_roots = dict()
-
 story_xml_trees = dict()
 story_xml_roots = dict()
-
-if design_map:
-    design_map_xml_tree = ET.parse(directory + design_map)
-    design_map_xml_root = design_map_xml_tree.getroot()
-
-for item in spreads:
-    tree = ET.parse(directory + "Spreads/" + item)
-    root = tree.getroot()
-    spread_xml_trees[item] = tree
-    spread_xml_roots[item] = root
-
-print("************************")
 
 for item in stories:
     tree = ET.parse(directory + "Stories/" + item)
@@ -66,8 +24,8 @@ for item in stories:
     story_xml_trees[item] = tree
     story_xml_roots[item] = root
 
-
 fout = open(output_html_file, 'w')
+
 html_template_start = """<HTML>
 <HEAD>
     <title>Test Generated from IDML</title>
@@ -76,7 +34,6 @@ html_template_start = """<HTML>
 <BODY>
 """
 fout.write(html_template_start)
-
 
 # walk through story_xml_roots and findall / search for appropriate children (or maybe iter for them)
 for key, value in story_xml_trees.items():
@@ -90,9 +47,4 @@ html_template_end = """</BODY>
 </HTML>"""
 fout.write(html_template_end)
 
-
-# for item in master_spreads:
-#     print(item)
-#
-# for item in stories:
-#     print(item)
+fout.close()
